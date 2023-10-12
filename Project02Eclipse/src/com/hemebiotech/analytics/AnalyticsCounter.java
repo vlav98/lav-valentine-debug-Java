f665329e4e0e7718e3b2b952d73dcc45d9846057
Valentine Lav <valentinelav98@gmail.com>
2023-10-12 21:07:18 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,11 +3,54 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+	public List<String> getSymptoms() {
+		return reader.GetSymptoms();
+	}
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> symptomCounts = new HashMap<>();
+
+		for (String symptom : symptoms) {
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
+		}
+
+		return symptomCounts;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		List<Map.Entry<String, Integer>> entryList = new ArrayList<>(symptoms.entrySet());
+		Collections.sort(entryList, (entry1, entry2) -> entry1.getKey().compareTo(entry2.getKey()));
+		Map<String, Integer> sortedSymptoms = new LinkedHashMap<>();
+
+		for (Map.Entry<String, Integer> entry : entryList) {
+			sortedSymptoms.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedSymptoms;
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.WriteSymptoms(symptoms);
+	}
 
 	public static void main(String args[]) throws Exception {
 		try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
